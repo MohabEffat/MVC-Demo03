2792dd98561d74856c15fb9c3ca5fedc16ce6eaf
Mohab <Mohabeffat76@gmail.com>
2024-09-02 01:40:06 +0300

    Add project files.

---
+++ Data.Models/Contexts/CompanyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //optionsBuilder.UseSqlServer("");
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Department> departments { get; set; }
+    }
+}
